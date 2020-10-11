e28c4182b910adfef90137e66110e86442471e50
cuibst <cbst987@126.com>
2020-10-11 20:38:22 +0800

    Add a basic unit test for news model and news parser #1

--- src/main/java/com/rzotgorz/service/NewsParser.java
+++ src/main/java/com/rzotgorz/service/NewsParser.java
@@ -16,8 +16,7 @@ public class NewsParser {
         List<String> contents = JSON.parseArray(jsonObject.getJSONArray("content").toJSONString(),String.class);
         String textContent = "";
         for(String line : contents) {
-            String tmp = line.substring(0,3);
-            if(tmp.equals("img_"))
+            if(line.indexOf("img_")==0)
                 continue;
             textContent = textContent + line.substring(5);
         }
