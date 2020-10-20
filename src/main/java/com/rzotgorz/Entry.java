a4572e64fb8f0133a5afbaae00b65751678c1379
cuibst <cbst987@126.com>
2020-10-20 21:00:34 +0800

    Modify initialization of index. #3

--- src/main/java/com/rzotgorz/Entry.java
+++ src/main/java/com/rzotgorz/Entry.java
@@ -28,63 +28,66 @@ public class Entry
     public static void initializeIndex() throws SQLException, IOException {
         System.out.println("Index Initializing...");
         DatabaseConnector connector = new DatabaseConnector();
-        ResultSet resultSet = connector.query("SELECT * FROM backend_news;");
-        int cnt = 0;
-        while(resultSet.next()) {
-            Directory dir = null;
-            boolean flag = false;
-            if(LuceneConfig.directoryExist()) {
+        int cnt = 20;
+        while(true)
+        {
+            String q = "SELECT * FROM backend_news WHERE id BETWEEN "+(cnt-20)+" AND "+cnt+';';
+            ResultSet resultSet = connector.query(q);
+            if(!resultSet.next())
+                break;
+            do {
+                Directory dir = null;
+                boolean flag = false;
+                if (LuceneConfig.directoryExist()) {
+                    try {
+                        dir = LuceneConfig.directory();
+                        IndexReader reader = DirectoryReader.open(dir);
+                        IndexSearcher searcher = new IndexSearcher(reader);
+                        //Use term because the id didn't go through analyzer when added.
+                        Query query = new TermQuery(new Term("id", resultSet.getString("news_id")));
+                        TopDocs topDocs = searcher.search(query, 10);
+                        reader.close();
+                        dir.close();
+                        if (topDocs.scoreDocs.length != 0)
+                            continue;
+                    } catch (IndexNotFoundException e) {
+                        flag = true;
+                    }
+                } else
+                    flag = true;
                 try {
                     dir = LuceneConfig.directory();
-                    IndexReader reader = DirectoryReader.open(dir);
-                    IndexSearcher searcher = new IndexSearcher(reader);
-                    //Use term because the id didn't go through analyzer when added.
-                    Query query = new TermQuery(new Term("id", resultSet.getString("news_id")));
-                    TopDocs topDocs = searcher.search(query, 10);
-                    reader.close();
+                    IndexWriterConfig config = new IndexWriterConfig(LuceneConfig.analyzer());
+                    if (flag) //When the index doesn't exist, create it instead of add into it.
+                        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+                    IndexWriter writer = new IndexWriter(dir, config);
+                    Document document = new Document();
+                    FieldType fieldType = LuceneConfig.fieldType();
+                    document.add(new Field("title", resultSet.getString("title"), fieldType));
+                    document.add(new Field("content", resultSet.getString("content"), fieldType));
+                    document.add(new Field("category", resultSet.getString("category"), fieldType));
+                    document.add(new Field("summary", resultSet.getString("summary"), fieldType));
+                    document.add(new Field("tags", resultSet.getString("tags"), fieldType));
+                    document.add(new StringField("id", resultSet.getString("news_id"), Field.Store.YES));
+                    document.add(new StringField("source", resultSet.getString("source"), Field.Store.YES));
+                    document.add(new StringField("news_url", resultSet.getString("news_url"), Field.Store.YES));
+                    document.add(new StringField("media", resultSet.getString("media"), Field.Store.YES));
+                    document.add(new StringField("pub_date", resultSet.getString("pub_date"), Field.Store.YES));
+                    document.add(new StringField("img", resultSet.getString("img"), Field.Store.YES));
+                    writer.addDocument(document);
+                    writer.close();
                     dir.close();
-                    if (topDocs.scoreDocs.length != 0)
-                        continue;
-                } catch (IndexNotFoundException e) {
-                    flag = true;
+                } catch (Exception e) {
+                    return;
                 }
-            }
-            else
-                flag = true;
-            try {
-                dir = LuceneConfig.directory();
-                IndexWriterConfig config = new IndexWriterConfig(LuceneConfig.analyzer());
-                if(flag) //When the index doesn't exist, create it instead of add into it.
-                    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-                IndexWriter writer = new IndexWriter(dir, config);
-                Document document = new Document();
-                FieldType fieldType = LuceneConfig.fieldType();
-                document.add(new Field("title", resultSet.getString("title"), fieldType));
-                document.add(new Field("content", resultSet.getString("content"), fieldType));
-                document.add(new Field("category", resultSet.getString("category"), fieldType));
-                document.add(new Field("summary", resultSet.getString("summary"), fieldType));
-                document.add(new Field("tags", resultSet.getString("tags"), fieldType));
-                document.add(new StringField("id", resultSet.getString("news_id"), Field.Store.YES));
-                document.add(new StringField("source", resultSet.getString("source"), Field.Store.YES));
-                document.add(new StringField("news_url", resultSet.getString("news_url"), Field.Store.YES));
-                document.add(new StringField("media", resultSet.getString("media"), Field.Store.YES));
-                document.add(new StringField("pub_date", resultSet.getString("pub_date"), Field.Store.YES));
-                document.add(new StringField("img", resultSet.getString("img"), Field.Store.YES));
-                writer.addDocument(document);
-                writer.close();
-                dir.close();
-            } catch (Exception e) {
-                return;
-            }
-            cnt += 1;
-            if(cnt >= 100)
-                break;
+            } while(resultSet.next());
+            cnt += 20;
         }
     }
 
     public static void main(String[] args) {
         try {
-            //initializeIndex();
+            initializeIndex();
         } catch (Exception e) {
             e.printStackTrace();
         }
