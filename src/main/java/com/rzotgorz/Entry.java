a4fce7469b25a5f284db1aa92a3d096a85fe61c4
cuibst <cbst987@126.com>
2020-11-01 22:11:43 +0800

    Add some debug features to result. #7

--- src/main/java/com/rzotgorz/Entry.java
+++ src/main/java/com/rzotgorz/Entry.java
@@ -1,20 +1,7 @@
 package com.rzotgorz;
 
-import com.rzotgorz.configuration.LuceneConfig;
-import com.rzotgorz.service.DatabaseConnector;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.FieldType;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.index.*;
-import org.apache.lucene.store.Directory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * The Entry class for the whole website.
